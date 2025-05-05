040b2c52f0209b0028a491111edd6c4a4a77cf93
cassiusbeep <cassius_hall@brown.edu>
2025-05-04 20:55:10 -0400

    working on portfolio and study case

---
+++ portfolio-status.js
@@ -0,0 +1,25 @@
+const statusList = [
+  "<span>happy&nbsp;</span><span>to&nbsp;</span><span>see&nbsp;</span><span>you!</span>",
+  "<span>a&nbsp;</span><span>designer</span><span></span><span></span>",
+  "<span>a&nbsp;</span><span>product&nbsp;</span><span>manager</span><span></span>",
+  "<span>a&nbsp;</span><span>linguist</span><span></span><span></span>",
+  "<span>a&nbsp;</span><span>tinkerer</span><span></span><span></span>",
+  "<span>a&nbsp;</span><span>game&nbsp;</span><span>designer</span><span></span>"
+];
+
+let index = 0;
+
+function statusUpdate() {
+  const pfStatus = document.getElementById("portfolio-status");
+  index = index + 1 >= statusList.length ? 0 : index + 1;
+  pfStatus.innerHTML = statusList[index];
+}
+
+const pfStatus = document.getElementById("portfolio-status");
+if (pfStatus) {
+  pfStatus.addEventListener("click", function () {
+    statusUpdate();
+  });
+} else {
+  console.log("title didnt work...");
+}
